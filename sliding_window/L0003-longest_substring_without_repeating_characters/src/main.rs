bb7bcf2046565cf112d51626fea75c4a1ef60cb0
CalebStandfield <54382087+CalebStandfield@users.noreply.github.com>
2025-11-21 19:14:08 -0700

    L0003

---
+++ sliding_window/L0003-longest_substring_without_repeating_characters/src/main.rs
@@ -0,0 +1,50 @@
+//! Leetcode: 0003-Longest Substring Without Repeating Characters
+//! Link: https://leetcode.com/problems/longest-substring-without-repeating-characters
+//!
+//! Category: Sliding Window / Hashing
+//! Level: Medium
+//! Runtime: 1 ms | Beats 63.49%
+
+use std::collections::HashMap;
+
+fn main() {
+    let l = length_of_longest_substring("abcabcbb".to_string());
+    println!("{:?}", l);
+
+    let l = length_of_longest_substring("bbbbb".to_string());
+    println!("{:?}", l);
+
+    let l = length_of_longest_substring("pwwkew".to_string());
+    println!("{:?}", l);
+}
+
+pub fn length_of_longest_substring(s: String) -> i32 {
+    let mut r = 0;
+    let mut l = 0;
+    let mut longest = 0;
+    if s.len() == 0 {
+        return 0;
+    }
+    if s.len() == 1 {
+        return 1;
+    }
+    let mut map = HashMap::new();
+    map.insert(s.chars().nth(l).unwrap(), l);
+    let chars: Vec<char> = s.chars().collect();
+    while r < s.len() {
+        let c1 = chars[l];
+        let c2 = chars[r];
+        if !map.contains_key(&c2) {
+            map.insert(c2, r);
+            r += 1;
+            longest = longest.max(r - l);
+        } else if r == l {
+            r += 1;
+        } else {
+            map.remove(&c1);
+            l += 1;
+        }
+    }
+
+    longest as i32
+}
