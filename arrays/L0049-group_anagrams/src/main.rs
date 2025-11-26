f7ebc79d08a8c30845755ef9dd0fb0b35f4487fa
CalebStandfield <54382087+CalebStandfield@users.noreply.github.com>
2025-11-26 14:28:20 -0700

    L0049 Valid Anagram

---
+++ arrays/L0049-group_anagrams/src/main.rs
@@ -0,0 +1,24 @@
+//! Leetcode: 0049-Group Anagrams
+//! Link: https://leetcode.com/problems/group-anagrams
+//! Category: arrays
+//! Level: medium
+//! Runtime: 0 ms | Beats 100.00%
+
+use std::collections::HashMap;
+
+fn main() {
+    println!("Hello, world!");
+}
+
+pub fn group_anagrams(strs: Vec<String>) -> Vec<Vec<String>> {
+    let mut map = HashMap::new();
+    for s in strs {
+        let mut frequencies = [0u8; 26];
+        for c in s.bytes() {
+            frequencies[(c - b'a') as usize] += 1;
+        }
+        map.entry(frequencies).or_insert_with(Vec::new).push(s);
+    }
+    map.into_values().collect()
+}
+
