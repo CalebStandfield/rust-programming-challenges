5daff41edb7880f0b77ae5d6250b68b7a1d0e831
CalebStandfield <54382087+CalebStandfield@users.noreply.github.com>
2025-11-30 22:22:18 -0700

    L0703 kth largest element in a stream

---
+++ kth/L0703-kth_largest_element_in_a_stream/src/main.rs
@@ -0,0 +1,62 @@
+//! Leetcode: 0703 kth largest element in a stream
+//! Link: https://leetcode.com/problems/kth-largest-element-in-a-stream
+//!
+//! Category: kth
+//! Level: easy
+//! Runtime: 0 ms | Beats 100.00%
+//!
+//! Method of Solving: If we only need 
+
+use std::cmp::Reverse;
+use std::collections::BinaryHeap;
+
+fn main() {
+    let k = 3;
+    let nums = vec![4, 5, 8, 2];
+    let mut obj = KthLargest::new(k, nums);
+    let mut vec = Vec::new();
+    vec.push(obj.add(3));
+    vec.push(obj.add(5));
+    vec.push(obj.add(10));
+    vec.push(obj.add(9));
+    vec.push(obj.add(4));
+    println!("{:?}", vec);
+}
+
+struct KthLargest {
+    k: i32,
+    heap: BinaryHeap<Reverse<i32>>,
+}
+
+/**
+ * `&self` means the method takes an immutable reference.
+ * If you need a mutable reference, change it to `&mut self` instead.
+ */
+impl KthLargest {
+    fn new(k: i32, nums: Vec<i32>) -> Self {
+        let mut kth_largest = KthLargest {
+            k,
+            heap: BinaryHeap::new(),
+        };
+
+        for num in nums {
+            kth_largest.add(num);
+        }
+
+        kth_largest
+    }
+
+    fn add(&mut self, val: i32) -> i32 {
+        self.heap.push(Reverse(val));
+        if self.heap.len() > self.k as usize {
+            self.heap.pop();
+        }
+        self.heap.peek().unwrap().0
+    }
+}
+
+// /**
+//  * Your KthLargest object will be instantiated and called as such:
+//  * let obj = KthLargest::new(k, nums);
+//  * let ret_1: i32 = obj.add(val);
+//  */
